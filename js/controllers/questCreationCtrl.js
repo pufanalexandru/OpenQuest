8e03018b64988479287c742378a158b4f577421e
Alexandru Pufan <pufanalexandru@gmail.com>
2015-12-30 23:13:58 +0200

    added placeholders and solved minor bug with datepicker

--- js/controllers/questCreationCtrl.js
+++ js/controllers/questCreationCtrl.js
@@ -1,7 +1,13 @@
 app.controller('questCreationCtrl', ['$scope', function ($scope) {
 
     $('.colorPicker').colorpicker({ format: 'hex' }); 
-    $('#datetimepicker').datetimepicker({ minDate: new Date() });   
+    $('#datetimepicker').datetimepicker({ 
+        minDate: new Date()
+    });   
+    
+    $scope.quest = {
+        deadline: ''
+    };
     
     $scope.createQuest = function () {
         console.log('asdsa');
