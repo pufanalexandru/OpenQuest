419006e069605ab26947fa654e0a5f10b8999106
Alexandru Pufan <pufanalexandru@gmail.com>
2015-12-30 13:33:50 +0200

    managed to collapse menu when you click a list item

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -3,4 +3,7 @@ app.controller('mainCtrl', ['$scope', 'auth', function ($scope, auth) {
     auth.checkToken();   
     $scope.logout = auth.logout;
     
+    $('#menu li').click(function () {
+        $('#menu').removeClass('in');
+    });
 }]);
