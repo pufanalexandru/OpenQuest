14bd61672c6165eb8ab5a7c10894d1c77a9871e1
Alexandru Pufan <pufanalexandru@gmail.com>
2015-12-30 18:13:56 +0200

    prevent datepicker to choose previous date

--- js/controllers/questCreationCtrl.js
+++ js/controllers/questCreationCtrl.js
@@ -1,9 +1,9 @@
 app.controller('questCreationCtrl', ['$scope', function ($scope) {
 
-    $('.colorPicker').colorpicker({format: 'hex'}); 
-    $('#datetimepicker').datetimepicker();
+    $('.colorPicker').colorpicker({ format: 'hex' }); 
+    $('#datetimepicker').datetimepicker({ minDate: new Date() });   
     
     $scope.createQuest = function () {
         console.log('asdsa');
-    }
+    };
 }]);
