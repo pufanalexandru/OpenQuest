360ee90a5b08656944cf6460c66c778e5459b101
Alexandru Pufan <pufanalexandru@gmail.com>
2015-12-31 13:03:14 +0200

    added toggle button for quest in adventures

--- js/controllers/questCreationCtrl.js
+++ js/controllers/questCreationCtrl.js
@@ -4,12 +4,23 @@ app.controller('questCreationCtrl', ['$scope', function ($scope) {
     $('#datetimepicker').datetimepicker({ 
         minDate: new Date()
     });   
+    $('#adventure').bootstrapToggle({
+        on: 'Yes',
+        off: 'No'
+    });
     
     $scope.quest = {
-        deadline: ''
+        deadline: '',
+        hasAdventure: false
+    };
+    
+    $scope.toggleAdventures = function () {
+        $scope.quest.hasAdventure = !$scope.quest.hasAdventure;
     };
     
     $scope.createQuest = function () {
         console.log('asdsa');
     };
+    
+
 }]);
