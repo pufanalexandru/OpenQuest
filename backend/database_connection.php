7f550c03b7b3fbacd896c26afeda59a9be6aaa3c
Alexandru Pufan <pufanalexandru@gmail.com>
2016-01-14 21:56:21 +0200

    removed php files from repository

--- backend/database_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $database_connection = mysqli_connect("127.0.0.1", "root", "", "taskit");
-    
-    if (!$database_connection) {
-        die("Connection failed!");
-    }
-?>
--- backend/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require('database_connection.php');
-    $token = mysqli_real_escape_string($database_connection, file_get_contents('php://input'));
-    
-    $query = "UPDATE users SET token = 'LOGGED OUT' WHERE token = '{$token}'";
-    $result = mysqli_query($database_connection, $query);
-?>
