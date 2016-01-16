e15bde5a5b52762d4dae593a654fb0dadb74e3e8
Alexandru Pufan <pufanalexandru@gmail.com>
2016-01-16 21:27:53 +0200

    changed animation in quest sidebar to angular style

--- js/controllers/questsCtrl.js
+++ js/controllers/questsCtrl.js
@@ -14,19 +14,9 @@ app.controller('questsCtrl', ['$scope', '$window', '$timeout', 'dataService', fu
         } else {
             $scope.statuses.push(status);
         }
-    };
-        
-    $scope.toggle = function () {
-        var length = $('#quest-content').css('margin-left') == "31px" ? 230 : 31;
-        
-        $('#quest-content').animate({ 'margin-left': length });
-        $('#quest-sidebar').animate({ 'width': length });
-        
-        $('#quest-sidebar i').toggleClass('fa-chevron-right');
-        $('#quest-sidebar').toggleClass('collapsed');
-    };
+    };       
     
     if ($window.innerWidth < 580) {
-        $scope.toggle();
+        $scope.collapsed = true;
     }
 }]);
